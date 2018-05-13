359cf36a50ba57e305ca26dcc8f6577d7c4c5b51
Michael Luttrell <michael.luttrell@gmail.com>
2018-05-13 10:20:03 -0700

    Added in body parsing and actually tested this code to verify that it in fact connects to postgres and creates the schema.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simpleRestToPostgres
 How simple can a ReST service be with just write and read
+
+This is a simple server that has two primary routes, a post to receive a message that will be stored in a postgres database.
+The body of the post message should be json in the format of:
+{ id: 'A unique String identifying this transaction',
+  transdate: 'A datetime that matches something postgres will recognize',
+  data: {message: 'Any json object will go in here' }
+}
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
-const http = require('http').Server(app)
+const express = require('express') // handles the ReST requests
+const app = express() // instantiate the joint.
+const bodyParser = require('body-parser') // so we can receive JSON POST requests.
+const http = require('http').Server(app) // this is a server, could be https but heroku will do that for us
 
-const port = process.env.PORT || 8081
-const connection = process.env.DATABASE_URL || 'postgresql://[::1]:5432/public'
+const port = process.env.PORT || 8081 // Heroku will provide the port
+const connection = process.env.DATABASE_URL || '[::1]:5432/public' // heroku will set the DATABSE_URL when adding it to a project.
 
-const knex = require('knex')({ client: 'pg', connection })
-const writeToDb = record => knex('transactions').insert(record)
+const knex = require('knex')({ client: 'pg', connection }) // for our postgres connection
+const writeToDb = record => knex('transactions').insert(record) // simple datbase write function.
 
-knex.schema.hasTable('transactions').then(function(exists) {
+app.use(bodyParser.json()) // yeah.. cause we really want that JSON
+
+// create a schema if it's not there... this is weird to have here but, trying to keep it simple.
+knex.schema.hasTable('transactions').then(function(exists) { // uh I think the code explains itself
   if (!exists) {
     return knex.schema.createTable('transactions', function(t) {
       t.string('id').primary()
@@ -35,4 +39,4 @@ app.get('/records', async (req, res) =>  {
   } catch ({message}) { res.status(500).send({success: false, message, transactions: []}) }
 })
 
-http.listen(port, () => console.log('listening on *:' + port) )
+http.listen(port, () => console.log('listening on *:' + port) ) // fire this puppy up.
