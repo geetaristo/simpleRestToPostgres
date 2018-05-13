016e0ba662c0265fc3bb821f2a429159c6dc1a10
Michael Luttrell <michael.luttrell@gmail.com>
2018-05-13 10:05:59 -0700

    Adding the source, the package.json and a .gitignore.

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const port = process.env.PORT || 8081
+
+const writeToDb = record => knex('transactions').insert(record)
+
+const connection = process.env.DATABASE_URL || 'postgresql://[::1]:5432/public'
+
+const knex = require('knex')({ client: 'pg', connection })
+
+knex.schema.hasTable('transactions').then(function(exists) {
+  if (!exists) {
+    return knex.schema.createTable('transactions', function(t) {
+      t.string('id').primary()
+      t.dateTime('transdate')
+      t.jsonb('data')
+    })
+  }
+})
+
+app.get('/', (req, res) => res.send('welcome to the root of this server.') )
+
+app.post('/record', async (req, res) => {
+  const { body } = req
+  try {
+    const result = await writeToDb(body.record)
+    res.send({success: true})
+  } catch ({message}) { res.status(500).send({success: false, message }) }
+})
+
+app.get('/records', async (req, res) =>  {
+  try {
+    const transactions = await knex('transactions').orderBy('transdate', 'desc')
+    res.send(transactions)
+  } catch ({message}) { res.status(500).send({success: false, message, transactions: []}) }
+})
+
+http.listen(port, () => console.log('listening on *:' + port) )
+
